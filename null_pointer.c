c9ea9c1091344471246126107386c3cc6c99522b
Iván Garcerant <igarcerant@gmail.com>
2022-11-19 15:08:23 -0500

    ver el caso en que funciona sin quitar el codigo que falla

--- null_pointer.c
+++ null_pointer.c
@@ -5,7 +5,8 @@ int main(int argc,char** argv)
 {
     int val = 1;
     int * p = NULL;
-    p = &val;
+    p = &val; // asi funciona bien
+    //*p = val; // y asi falla
     printf("%d\n", *p);
     return EXIT_SUCCESS;
 }
